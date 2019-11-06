58a69db25a80c3f48cd00dd99b1b0e9368b4faa1
Alex Shearer <alex.shearer23@gmail.com>
2019-11-06 13:44:04 -0600

    Added calculator component and styled app and calculator component

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Calculator.css'
+
+class Calculator extends Component {
+    render() {
+        return(
+            <h1 className="Calculator"></h1>
+        );
+    }
+}
+
+export default Calculator; 
