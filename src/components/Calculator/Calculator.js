8ed61a1b707f32863a000703037c1812f412fc54
Alex Shearer <alex.shearer23@gmail.com>
2019-11-07 13:39:55 -0600

    Added Keypad component and the keypadrow component and styled them

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Calculator.css'
 import Screen from '../Screen/Screen';
+import Keypad from '../Keypad/Keypad';
 
 class Calculator extends Component {
     render() {
         return(
             <div className="Calculator">
                 <Screen />
+                <Keypad />
             </div>
         );
     }
--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './Screen.css';
 import ResultsScreen from '../ResultsScreen/ResultsScreen';
+import ComputationScreen from '../ComputationScreen/ComputationScreen';
 
 class Screen extends Component {
     render() {
         return (
             <div className="Screen">
-                <ResultsScreen>
-                    0
-                </ResultsScreen>
+                <ResultsScreen>0</ResultsScreen>
+                <ComputationScreen> 2 + 3 </ComputationScreen>
             </div>
         );
     }
